d4923f9cadd241e25db28505b6ec67a95322a06e
Moh'd Khier Abualruz <mkh.ruzz@live.com>
2022-10-04 17:57:34 +0200

    upgrade images to php 7.4
    - adding more generic way to install xdebug and tideways xhprof extentions

--- build.sh
+++ build.sh
@@ -22,24 +22,24 @@ docker rmi "${phpImg}:${phpTag}"
 docker rmi "${phpFpmImg}:${phpFpmTag}"
 docker rmi "${phpJobRunnerImg}:${phpJobRunnerTag}"
 ## Building Core bullseye image
-echo "Building Core bullseye image"
-docker build --no-cache . -f bullseye/Dockerfile -t "${imgFull}"
+echo "Building Core bullseye image ${imgFull}"
+docker build . -f bullseye/Dockerfile -t "${imgFull}"
 ## Tagging Core bullseye image
-echo "Tagging Core bullseye image"
+echo "Tagging Core bullseye image ${img}:latest and ${imgTag}:latest"
 docker tag "${imgFull}" "${img}:latest"
 docker tag "${imgFull}" "${imgTag}:latest"
 ## Building Apache2 bullseye image
-echo "Building Apache2 bullseye image"
-docker build --no-cache . -f apache2/Dockerfile -t "${apache2Img}:${apache2Tag}"
+echo "Building Apache2 bullseye image ${apache2Img}:${apache2Tag}"
+docker build . -f apache2/Dockerfile -t "${apache2Img}:${apache2Tag}"
 ## Building php7.4 bullseye image
-echo "Building php7.4 bullseye image"
-docker build --no-cache . -f php74/Dockerfile -t "${phpImg}:${phpTag}"
+echo "Building php7.4 bullseye image ${phpImg}:${phpTag}"
+docker build . -f php74/Dockerfile -t "${phpImg}:${phpTag}"
 ## Building php7.4 FPM bullseye image
-echo "Building php7.4 FPM bullseye image"
-docker build --no-cache . -f fpm/Dockerfile -t "${phpFpmImg}:${phpFpmTag}"
+echo "Building php7.4 FPM bullseye image ${phpFpmImg}:${phpFpmTag}"
+docker build . -f fpm/Dockerfile -t "${phpFpmImg}:${phpFpmTag}"
 ## Building php7.4 Job Runner bullseye image
-echo "Building php7.4 Job Runner bullseye image"
-docker build --no-cache . -f jobrunner/Dockerfile -t "${phpJobRunnerImg}:${phpJobRunnerTag}"
+echo "Building php7.4 Job Runner bullseye image ${phpJobRunnerImg}:${phpJobRunnerTag}"
+docker build . -f jobrunner/Dockerfile -t "${phpJobRunnerImg}:${phpJobRunnerTag}"
 
 ## Remove unused images
 docker rmi "${imgFull}"
